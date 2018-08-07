ff02b4a8d75536c735edbcc23ccb058ac8672854
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-08-07 18:14:25 +0200

    Add todos field to a TodoListType GraphQL

--- pkg/models/todo_list.go
+++ pkg/models/todo_list.go
@@ -53,4 +53,20 @@ func init() {
 			return user, nil
 		},
 	})
+	TodoListType.AddFieldConfig("todos", &graphql.Field{
+		Type:        graphql.NewList(TodoType),
+		Description: "Get users Todos",
+		Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+			var todos []Todo
+
+			db := params.Context.Value("db").(*gorm.DB)
+			// if db := params.Context.Value(k); v != nil {
+			// 	fmt.Println("found value:", v)
+			// 	return
+			// }
+
+			db.Order("created_at asc").Where("todo_list_id = ?", params.Source.(TodoList).ID).Find(&todos)
+			return todos, nil
+		},
+	})
 }
